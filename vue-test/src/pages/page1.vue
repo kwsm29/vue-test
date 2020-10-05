2265774c39bb1f0177a914c446af366caf7cee7b
kwsm29 <m-kawashima@dji.co.jp>
2020-10-05 20:40:25 +0900

    v-for 画像表示

--- vue-test/src/pages/page1.vue
+++ vue-test/src/pages/page1.vue
@@ -1,20 +1,44 @@
 <template>
     <main class="l-main">
-      <p>トップページです</p>
+      <div class="l-container">
+        <ul class="p-list">
+          <li v-for="item in animal" :key="item.id" class="c-card">
+            <figure><img :src="item.img" alt=""></figure>
+            {{ item.name }}
+          </li>
+        </ul>
+        <ul class="p-list">
+          <li v-for="item in fruits" :key="item.id" class="c-card">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
     </main>
 </template>
 
-<style scoped>
- p {
-     color: red;
- }
+<style>
 </style>
 
-<script>
+<script defer>
+import Vue from 'vue'
+//コンポーネントのscriptは eport defaultde で囲む
 export default {
 　　data() {
       return {
           title: 'toop',
+          animal: [
+            {name: 'いぬ',
+            img : require('@/assets/images/dog.png')}
+            // 'ねこ',
+            // 'うさぎ',
+            // 'ぞう',
+          ],
+          fruits: [
+            'りんご',
+            'みかん',
+            'ぶどう',
+            'いちご',
+          ],
       }
   },
   head: {
@@ -32,4 +56,5 @@ export default {
     },
   }
 }
+
 </script>
