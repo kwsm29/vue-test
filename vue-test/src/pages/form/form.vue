c328f2295e6a3f496bf3864f67e41e453e9aada2
kwsm29 <m-kawashima@dji.co.jp>
2020-10-13 18:36:45 +0900

    ドメイン補完

--- vue-test/src/pages/form/form.vue
+++ vue-test/src/pages/form/form.vue
@@ -27,12 +27,18 @@
           </div>
           <div class="group">
             <label for="name" class="required">電話番号</label>
-            <input type="tel" v-model="form.tel"  autocomplete="tel" placeholder="電話番号" @blur="$v.form.tel.$touch()" :class="{invalid: $v.form.tel.$error}" class="formItem">
-            <small v-if="$v.form.tel.$error">電話番号を入力してください</small>
+            <input type="tel" v-model="form.tel" maxlength="11"  autocomplete="tel" placeholder="電話番号" @blur="$v.form.tel.$touch()" :class="{invalid: $v.form.tel.$error}" class="formItem">
+            <div v-if="$v.form.tel.$error">
+                <small v-if="!$v.form.tel.required">電話番号が入力されていません。</small>
+                <small v-if="!$v.form.tel.numeric">電話番号の形式が正しくありません。</small>
+            </div>
           </div>
           <div class="group">
             <label for="name" class="required">メールアドレス</label>
-            <input type="email" v-model="form.email"  autocomplete="email" placeholder="メールアドレス" autocorrect="off" autocapitalize="off" @blur="$v.form.email.$touch()" :class="{invalid: $v.form.email.$error}" class="formItem">
+            <input type="email" v-model="form.email"  autocomplete="email" placeholder="メールアドレス" autocorrect="off" autocapitalize="off" @blur="$v.form.email.$touch()" :class="{invalid: $v.form.email.$error}" class="formItem" list="suggestions">
+            <datalist id="suggestions">
+              <option v-for="domain in domains" :value="getSuggest(domain)"></option>
+            </datalist>
             <div v-if="$v.form.email.$error">
                 <small v-if="!$v.form.email.required">メールドレスが入力されていません。</small>
                 <small v-if="!$v.form.email.email">メールアドレスの形式が正しくありません。</small>
@@ -50,23 +56,38 @@
 
 <script>
 
-import { required ,email } from 'vuelidate/lib/validators'
+import { required ,email,numeric } from 'vuelidate/lib/validators'
 
 export default {
 　　data() {
       return {
           title: 'contact',
+          domains: [
+            "@gmail.com",
+            "@yahoo.co.jp",
+            "@ezweb.ne.jp",
+            "@docomo.ne.jp",
+            "@softbank.ne.jp"
+          ]
       }
   },
   validations: {
       form: {
         familyName: {required},
         givenName: {required},
-        tel: {required},
+        tel: {required,numeric},
         email: {required,email},
       },
   },
   methods: {
+    getSuggest: function (domain) {
+      if ((this.form.email.match(/@/g) || []).length === 1) {
+        const emailUserName = this.form.email.split('@')[0]
+        return emailUserName + domain
+      } else {
+        return null
+      }
+    },
     submitForm(){
         this.$v.$touch();
         if(this.$v.$invalid){
